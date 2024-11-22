4faa5130b3c76b7db36da0137ad98b6ec4b1d9a8
Eliott Lacomme Ruiz <EliottLacommeRuiz2027@u.northwestern.edu>
2024-11-22 17:11:12 -0600

    general info

---
+++ Subscale/Subscale/Subscale.ino
@@ -0,0 +1,20 @@
+/* Notes
+  - Will be using a Teensy 4.1 to log data, that will be the primary purpose 
+  - Using a BNO085 to get IMU data
+    - Want angles, velocity, apogee reached, max velocity, orientation at landing
+  - Using a BMP390 for alitutde calculation 
+    - Want maximum altitude (apogee) (have both BNO integrate and BMP for comparisson -- 3 apogee comparissons)
+  - Time of landing 
+    - Will use on board clock on MCU to keep track of time 
+  - Will need to log data
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop()  {
+  // put your main code here, to run repeatedly:
+
+}
